e3eacde283ddca14300d4cd509df3cb70313d627
Santiago Zarate <santiago@zarate.co>
2023-02-02 17:58:13 +0100

    [rustfmt] Changes from rustfmt

--- duplicate_detection/src/lib.rs
+++ duplicate_detection/src/lib.rs
@@ -1,16 +1,18 @@
 //use std::fs::{self, DirEntry, ReadDir};
 use std::fs;
-use std::path::Path;
 use std::io;
-use std::io::{Error,Write};
+use std::io::{Error, Write};
+use std::path::Path;
 pub fn get_files() -> Result<Vec<File>, Error> {
     //#let files;
-    let dir; 
+    let dir;
     let mut file_list = vec![];
     dir = fs::read_dir(".").unwrap();
     for entries in dir {
         if let Ok(entries) = entries {
-            file_list.push(File { name: entries.path().display().to_string() });
+            file_list.push(File {
+                name: entries.path().display().to_string(),
+            });
         }
     }
 
@@ -21,10 +23,8 @@ pub struct File {
     name: String,
 }
 
-
 pub fn print() -> io::Result<()> {
-
-let mut stdout = io::stdout();
+    let mut stdout = io::stdout();
 
     let mut file_list = vec![];
     file_list = get_files().unwrap();
