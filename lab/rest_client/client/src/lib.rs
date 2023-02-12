ef88a6c71a4fa0403f4c5183e11674fd3728b39e
Santiago Zarate <santiago@zarate.co>
2023-02-13 00:48:21 +0100

    client/Cargo.toml

--- lab/rest_client/client/src/lib.rs
+++ lab/rest_client/client/src/lib.rs
@@ -1,7 +1,7 @@
-#![allow(unused_variables)]
+#![allow(unused_variables, dead_code)]
 fn main() {
-#[no_mangle]
-pub extern "C" fn hello_world() {
-    println!("Hello World! FROM RUST BABY!!!");
-}
+    #[no_mangle]
+    pub extern "C" fn hello_world() {
+        println!("Hello World! FROM RUST BABY!!!");
+    }
 }
