1a6e0cef6acf889a8662dc611733a7979735fa01
Santiago Zarate <santiago@zarate.co>
2023-08-06 00:21:23 +0200

    Cleanup

--- lab/rest_client/build.sh
+++
@@ -1,5 +0,0 @@
-cd /Users/foursixnine/Projects/foursixnine.io/cndk8/lab/rest_client
-find . -path ./dependencies -prune -false -iwholename '*cmake*' -not -name CMakeLists.txt -delete
-rm -rf build
-mkdir build
-cd build && cmake ..
--- lab/rest_client/client/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![allow(unused_variables, dead_code)]
-fn main() {
-    #[no_mangle]
-    pub extern "C" fn hello_world() {
-        println!("Hello World! FROM RUST BABY!!!");
-    }
-}
